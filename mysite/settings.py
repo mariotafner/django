0c3acc0412d73a569469566a78ae587028c55110
Mario Fabre <66124986+mariotafner@users.noreply.github.com>
2022-09-08 10:37:28 -0300

    PGDATABASE

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,14 +73,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': os.environ["PGDATABASE"],
+#        'USER': os.environ["PGUSER"],
+#        'PASSWORD': os.environ["PGPASSWORD"],
+#        'HOST': os.environ["PGHOST"],
+#        'PORT': os.environ["PGPORT"],
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'clubeassistenc01',
+        'USER': 'clubeassistenc01',
+        'PASSWORD': 'MaqLoc10975',
+        'HOST': 'mysql.clubeassistencia.com.br',
+        'PORT': '3306',
     }
 }
 
